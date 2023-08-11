5ab96aba153b9caa709e7378aeb5ea6856503760
Andy Xie <andy.xie.opensource@gmail.com>
2023-08-11 18:52:03 +0800

    api.py: Initial implementation

--- leetcode_0001_two_sum/api.py
+++ leetcode_0001_two_sum/api.py
@@ -1,7 +1,26 @@
 """API for solving problem Two Sum"""
 
-def _check_preconditions(...) -> bool:
-    pass
+LEN_MAX = 10**4
+LEN_MIN = 2
+
+NUM_MAX = 10**9
+NUM_MIN = -NUM_MAX
+
+TARGET_MAX = NUM_MAX
+TARGET_MIN = NUM_MIN
+
+def _check_preconditions(nums: list[int], target: int) -> bool:
+    if not LEN_MIN <= len(nums) <= LEN_MAX:
+        return False
+
+    for x in nums:
+        if not NUM_MIN <= x <= NUM_MAX:
+            return False
+
+    if not TARGET_MIN <= target <= TARGET_MAX:
+        return False
+
+    return True
 
 
 def two_sum(...) -> ...:
