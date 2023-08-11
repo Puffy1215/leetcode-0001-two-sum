226cbea4e34fb83018c6ff0b9ec5ba5ce4efb8e8
Andy Xie <andy.xie.opensource@gmail.com>
2023-08-11 18:11:20 +0800

    test_api.py: Add test values

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,8 +8,9 @@ from leetcode_0001_two_sum import api
 @pytest.mark.parametrize(
     ["result", "nums", "target"],
     (
-        [..., ...],
-        [..., ...],
+        [[0, 1], [2, 7, 11, 15], 9],
+        [[1, 2], [3, 2, 4], 6],
+        [[0, 1], [3, 3], 6],
     ),
 )
 def test_two_sum(result, nums: list[int], target: int) -> None:
